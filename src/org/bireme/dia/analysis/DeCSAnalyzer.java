d7bafa240cb1af83ec58651ec675caa01a1ef976
heitorbarbieri <barbieri@paho.org>
2016-02-12 09:27:18 -0200

    BVSStandardAnalyzer with WordDelimiterFilter

--- src/org/bireme/dia/analysis/DeCSAnalyzer.java
+++ src/org/bireme/dia/analysis/DeCSAnalyzer.java
@@ -8,7 +8,6 @@ import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.KeywordTokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
-import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
 
 public class DeCSAnalyzer extends Analyzer {
     public static final boolean WORDS = true;
@@ -19,7 +18,6 @@ public class DeCSAnalyzer extends Analyzer {
     public static final boolean ONLYQLF = false;
 
     private final SynonymEngine engine;
-    private final int wordDelimiterConfig;
 
     public DeCSAnalyzer() throws IOException {
         final ClassLoader loader = this.getClass().getClassLoader();
@@ -28,14 +26,6 @@ public class DeCSAnalyzer extends Analyzer {
         try {
             engine = new DeCSEngine("resources/decs/main", CATEGORY, SYN, 
                                                                KEYQLF, ONLYQLF);
-            wordDelimiterConfig = WordDelimiterFilter.GENERATE_WORD_PARTS +
-                                  WordDelimiterFilter.GENERATE_NUMBER_PARTS +
-                                //WordDelimiterFilter.CATENATE_WORDS +
-                                //WordDelimiterFilter.CATENATE_NUMBERS +
-                                  WordDelimiterFilter.SPLIT_ON_NUMERICS +
-                                  WordDelimiterFilter.STEM_ENGLISH_POSSESSIVE +
-                                  WordDelimiterFilter.CATENATE_ALL;        
-                                //WordDelimiterFilter.PRESERVE_ORIGINAL;
         } catch (Exception ioe) {
             System.err.println("dirUrl=" + dirUrl);
             System.err.println("error=" + ioe.toString());
@@ -50,11 +40,9 @@ public class DeCSAnalyzer extends Analyzer {
         final TokenStream filter2 = new LowerCaseFilter(filter1);
         final TokenStream filter3 = new SynonymFilter(filter2, engine,
                                                                  WORDS, PRECOD);
-        final TokenStream filter4 =  new WordDelimiterFilter(filter3, 
-                                                 wordDelimiterConfig, null);
-        final TokenStream filter5 = new LowerCaseFilter(filter4);
-        final TokenStream filter6 = new ASCIIFoldingFilter(filter5);
+        final TokenStream filter4 = new LowerCaseFilter(filter3);
+        final TokenStream filter5 = new ASCIIFoldingFilter(filter4);
 
-        return new TokenStreamComponents(source, filter6);
+        return new TokenStreamComponents(source, filter5);
     }
 }
